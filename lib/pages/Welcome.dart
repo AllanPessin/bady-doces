1a8713b752ecb245f4d0129b32c807f67ec2610e
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-03-09 14:43:03 -0300

    Fix Welcome code

--- lib/pages/Welcome.dart
+++ lib/pages/Welcome.dart
@@ -1,82 +1,111 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class Welcome extends StatefulWidget {
-//   @override
-//   _WelcomeState createState() => _WelcomeState();
-// }
+class Welcome extends StatefulWidget {
+  @override
+  _WelcomeState createState() => _WelcomeState();
+}
 
-// class _WelcomeState extends State<Welcome> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Material(
-//       color: Colors.lightBlueAccent[400],
-//       child: Column(
-//         children: [
-//           SizedBox(
-//             height: 60.0,
-//           ),
-//           Image(
-//             image: AssetImage('lib/assets/logo.png'),
-//             height: 150.0,
-//           ),
-//           SizedBox(
-//             height: 200.0,
-//           ),
-//           Container(
-//             decoration: BoxDecoration(
-//               boxShadow: [
-//                 BoxShadow(
-//                   color: Colors.black.withOpacity(0.5),
-//                   blurRadius: 10.0,
-//                   spreadRadius: 3.0,
-//                   offset: Offset(5, 3),
-//                 ),
-//               ]
-//             ),
-//             child: FlatButton(onPressed: () => {},
-//              child: Text('Entrar',
-//               style: TextStyle(
-//                 fontSize: 14,
-//                 fontWeight: FontWeight.w600,
-//               ),
-//             ),
-//             color: Colors.white,
-//             shape: RoundedRectangleBorder(
-//             borderRadius: BorderRadius.circular(10)
-//             ),
-//             padding: EdgeInsets.fromLTRB(100, 15, 100, 15), 
-//             ),
-//           ),
-//           SizedBox(
-//             height: 20.0,
-//           ),
-//           Container(
-//             decoration: BoxDecoration(
-//               boxShadow: [
-//                 BoxShadow(
-//                   color: Colors.black.withOpacity(0.5),
-//                   blurRadius: 10.0,
-//                   spreadRadius: 3.0,
-//                   offset: Offset(5, 3),
-//                 ),
-//               ]
-//             ),
-//             child: FlatButton(onPressed: () => {},
-//              child: Text('Cadstrar-se',
-//               style: TextStyle(
-//                 fontSize: 14,
-//                 fontWeight: FontWeight.w600,
-//               ),
-//             ),
-//             color: Colors.lightBlueAccent,
-//             shape: RoundedRectangleBorder(
-//             borderRadius: BorderRadius.circular(10)
-//             ),
-//             padding: EdgeInsets.fromLTRB(82, 15, 82, 15), 
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Cor de fundo do app definido por backgroundColor
+      backgroundColor: Colors.lightBlueAccent[400],
+      //Elementos roganizados em coluna dentro de um container ocupando toda a tela do dispositi
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(          
+          //Array de filhos do elemento coluna
+          children: <Widget>[
+            //Containar utilizadp para definir a logo
+            Container(
+              margin: EdgeInsets.only(top: 60, left: 90, right: 90),
+              child: Image.asset("lib/assets/logo.png"),                
+            ),
+            //Widgets button não são personalizaveis, por isso a necessidade de um container
+            //Botão para acessar conta no app
+
+            Container(
+              //estabelce distancia de outro container
+              margin: EdgeInsets.only(top: 240),
+              //definindo altura e largura do container
+              width: MediaQuery.of(context).size.width * .7,
+              height:  MediaQuery.of(context).size.width * .14,        
+              //alinhando em relaçao ao body    
+              alignment: Alignment.center,
+              //decoraçao de container por meio da classe a baixo
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    blurRadius: 10.0,
+                    spreadRadius: 3.0,
+                    offset: Offset(5, 3)
+                  )
+                ],
+                //Container precisa da border radius para a borderShadow ficar redonda
+                borderRadius: BorderRadius.all(
+                  Radius.circular(40)
+                )
+              ),
+              //SizedBox.expand faz o elemento ocupar todo o espaço dentro do container
+              child: SizedBox.expand(
+              //button inserido dentro do container ja estilizado
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(40)
+                  )
+                ),
+                color: Colors.white,
+                height: MediaQuery.of(context).size.height * .14,
+                child: Text(
+                  "Entrar", textAlign: TextAlign.center,
+                ),
+                onPressed: () => {  },
+                ),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              width: MediaQuery.of(context).size.width * .7,
+              height: MediaQuery.of(context).size.width * .14,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    blurRadius: 10.0,
+                    spreadRadius: 3.0,
+                    offset: Offset(5, 3)
+                  )
+                ],
+                borderRadius: BorderRadius.all(
+                  Radius.circular(40)
+                )
+              ),
+              child: SizedBox.expand(
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(40)
+                  )
+                ),
+                color: Colors.lightBlue,
+                height: MediaQuery.of(context).size.height * .14,
+                hoverColor: Colors.orangeAccent,
+                child: Text(
+                  "Cadastre-se", textAlign: TextAlign.center,
+                ),
+                onPressed: () => {  },
+                ),
+              ),
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
